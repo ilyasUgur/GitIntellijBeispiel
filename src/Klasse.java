4cffd32c1db210e0b0ecf2cb3a50a24a922e66e0
ilyasUgur <83614000+ilyasUgur@users.noreply.github.com>
2021-08-21 16:20:54 +0200

    Version 1:eigene Übungungen

--- src/Klasse.java
+++ src/Klasse.java
@@ -2,5 +2,7 @@ public class Klasse {
     public static void main(String[] args) {
         System.out.println("ich versuche eigene Hausaufgabe fertigzumachen");
         System.out.println("Abwarten und Tee trinken");
+
+        System.out.println("Eile mit Weile");
     }
 }
