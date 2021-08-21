3e245ce63d5046e1238db57a1abd67d31f68fc3c
ilyasUgur <83614000+ilyasUgur@users.noreply.github.com>
2021-08-21 19:24:26 +0200

    Version 2:eigene Übungungenn

--- src/Noten.java
+++ src/Noten.java
@@ -4,5 +4,6 @@ public class Noten {
         System.out.println("Er faengt bei Null an");
         System.out.println("es ist nicht alles Gold was glaenzt");
         System.out.println("Auf Kinder wirken das Virbild nicht Kritik");
+        System.out.println("Alles im Griff");
     }
 }
